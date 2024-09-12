f0b3c5584bfc7b5124da57011984e910cc5e4c73
Robert-liu3 <robert.liu3@mail.mcgill.ca>
2024-09-12 18:49:30 -0400

    Update q1.py

--- src/q1.py
+++ src/q1.py
@@ -2,15 +2,25 @@ import numpy as np
 from ucimlrepo import fetch_ucirepo 
   
 def fetch_data():
+
+    ### Infrared Thermography Temperature ###
     # fetch dataset 
     infrared_thermography_temperature = fetch_ucirepo(id=925) 
-
     # data (as pandas dataframes) 
     X = infrared_thermography_temperature.data.features 
     y = infrared_thermography_temperature.data.targets 
 
+    ### CDC Diabetes Health Indicators ###
+    # fetch dataset 
+    cdc_diabetes_health_indicators = fetch_ucirepo(id=891) 
+    # data (as pandas dataframes) 
+    X = cdc_diabetes_health_indicators.data.features 
+    y = cdc_diabetes_health_indicators.data.targets 
+
     # metadata 
     print(infrared_thermography_temperature.metadata) 
+    print(cdc_diabetes_health_indicators.metadata) 
 
     # variable information 
     print(infrared_thermography_temperature.variables) 
+    print(cdc_diabetes_health_indicators.variables) 
