321415adcf5f3cf3b18a8d0787b943b6c2b38a4e
Robert-liu3 <robert.liu3@mail.mcgill.ca>
2024-09-13 18:11:05 -0400

    finished q1 part 1 and part 2

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from q1 import fetch_data
+from q1.ITT_q1 import fetch_data_ITT
 
 def main():
     print("Hello World!")
 
 if __name__ == "__main__":
     main()
-    fetch_data()
+    fetch_data_ITT()
---
+++ src/q1/CDC_q1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+from ucimlrepo import fetch_ucirepo 
+from sklearn.preprocessing import LabelEncoder
+
+def fetch_data_CDC():
+
+    ### CDC Diabetes Health Indicators ###
+    # fetch dataset 
+    cdc_diabetes_health_indicators = fetch_ucirepo(id=891) 
+    # data (as pandas dataframes) 
+    X_CDC = cdc_diabetes_health_indicators.data.features 
+    Y_CDC = cdc_diabetes_health_indicators.data.targets
+    # metadata  
+    print(cdc_diabetes_health_indicators.metadata) 
+    # variable information 
+    print(cdc_diabetes_health_indicators.variables) 
