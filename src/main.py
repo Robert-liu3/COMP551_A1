8e0b338b1f3c24f59b2f18eb38451a742b3dfa8c
Robert-liu3 <77682309+Robert-liu3@users.noreply.github.com>
2024-09-16 17:55:31 -0400

    progress on linear regression

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from q1.ITT import fetch_data_ITT
+from q2.LinearRegression import LinearRegression
 
 def main():
     print("Hello World!")
 
 if __name__ == "__main__":
+    model = LinearRegression()
     main()
-    fetch_data_ITT()
+    x,y = fetch_data_ITT()
+    model.fit(x,y)
+
