17528828ec17979b95e8d495435dfa469b928ce5
NoahYe123 <jian.ye@mail.mcgill.ca>
2024-09-16 21:16:26 -0400

    updated CDC

--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
 from q1.ITT import fetch_data_ITT, check_data
 from q1.CDC import fetch_data_CDC
-from q2.LinearRegression import LinearRegression
+
 
 def main():
     print("Hello World!")
 
 if __name__ == "__main__":
     # model = LinearRegression()
+    print("Hello World!")
     main()
-    # x,y = fetch_data_ITT()
-    # check_data(x,y)
+    x,y = fetch_data_ITT()
+    check_data(x,y)
     # model.fit(x,y)
     # fetch_data_CDC()
 
