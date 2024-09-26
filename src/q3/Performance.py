d967c85781f013b29a04b4e4a99caa9c3346fed8
Robert-liu3 <robert.liu3@mail.mcgill.ca>
2024-09-25 21:36:42 -0400

    Update Performance.py

--- src/q3/Performance.py
+++ src/q3/Performance.py
@@ -1,10 +1,13 @@
 from q2.LinearRegression import LinearRegression
 from q2.LogisticRegression import LogisticRegression
+from q1.ITT import fetch_data_ITT
+from q1.CDC import fetch_data_CDC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import matplotlib.pyplot as plt
 
-def performanceLinearRegression(x,y,size=0.2):
+def performanceLinearRegression(size=0.2):
+    x,y = fetch_data_ITT()
     X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=size, random_state=42)
 
     model = LinearRegression(add_bias = False)
@@ -17,7 +20,8 @@ def performanceLinearRegression(x,y,size=0.2):
     plt.title('Performance of the model with test size = {}'.format(size))
     plt.show()
 
-def performanceLogisticRegression(x,y,size = 0.2):
+def performanceLogisticRegression(size = 0.2):
+    x,y = fetch_data_CDC()
     X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=size, random_state=42)
 
     model = LogisticRegression(verbose=True)
